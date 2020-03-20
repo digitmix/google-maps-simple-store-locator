bca80cf8730d27b5f9bbe204d18f8ba520b15d17
digitmix <bartek.gackiewicz@gmail.com>
2020-03-20 10:25:58 +0100

    coordinates

--- src/app.js
+++ src/app.js
@@ -111,8 +111,8 @@ function sanitizeHTML(strings) {
 function initMap() {
   // Create the map.
   const map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 7,
-    center: {lat: 52.632469, lng: -1.689423},
+    zoom: 11,
+    center: {lat: 51.25, lng: 22.56667},
     styles: mapStyle,
   });
 
